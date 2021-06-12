b50a03269d85bdcce6d389dbdb9d32ef6660b7f7
ahmtcnsvr <ahmettcansever@gmail.com>
2021-06-12 20:07:50 +0200

    update

---
+++ projects/202-Terraform-Phonebook-Application-deployed-on-AWS/sec-gr.tf
@@ -0,0 +1,67 @@
+resource "aws_security_group" "server-sg" {
+  name   = "WebServerSecurityGroup"
+  vpc_id = "vpc-384bf645"
+  tags = {
+    "Name" = "TF_WebServerSecurityGroup"
+  }
+  ingress {
+    from_port       = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb-sg.id]
+    to_port         = 80
+  }
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    to_port     = 22
+  }
+  egress {
+    from_port   = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+    to_port     = 0
+  }
+}
+
+resource "aws_security_group" "alb-sg" {
+  name   = "ALBSecurityGroup"
+  vpc_id = "vpc-384bf645"
+  tags = {
+    "Name" = "ALBSecurityGroup"
+  }
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    to_port     = 80
+  }
+  egress {
+    from_port   = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+    to_port     = 0
+  }
+}
+
+resource "aws_security_group" "db-sg" {
+  name   = "RDSSecurityGroup"
+  vpc_id = "vpc-384bf645"
+  tags = {
+    "Name" = "RDSSecurityGroup"
+  }
+  ingress {
+    from_port       = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.server-sg.id]
+    to_port         = 3306
+  }
+  egress {
+    from_port   = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+    to_port     = 0
+  }
+
+
+}
